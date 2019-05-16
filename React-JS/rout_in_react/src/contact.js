8fa5bd6a5cb704f991622d941470d04a57eb70b6
Avinash Sharma <avi.rohit13@hotmail.com>
2019-05-16 16:36:28 +0530

    Second Commit Of the day

--- React-JS/rout_in_react/src/contact.js
+++ React-JS/rout_in_react/src/contact.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 class Contact extends React.Component {
+  onSubmit = () => {
+    this.props.history.push("/");
+  };
+
   render() {
-    return <h1>Contact</h1>
+    return (
+      <form>
+        <input placeholder="name" type="name" />
+        <input placeholder="name" type="email" />
+        <button onClick={this.onSubmit}>Submit</button>
+      </form>
+    );
   }
 }
 
-export default Contact
+export default Contact;
--- React-JS/rout_in_react/src/index.js
+++ React-JS/rout_in_react/src/index.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import {
-    Route,
-    Link,
-    BrowserRouter as Router
-} from 'react-router-dom';
-import App from './App';
-import Contact from './contact';
-import Users from './users';
-import * as serviceWorker from './serviceWorker';
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import App from "./App";
+import Users from "./users";
+import Contact from "./contact";
+import Notfound from "./notfound";
 
-const routing=(
-    <Router>
-        <div>
-            <Route path="/" component={App}/>
-            <Route path="./users" component={Users}/>
-            <Route path="./contact" component={Contact}/>
-        </div>
-    </Router>
-)
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/users">
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById("root"));
